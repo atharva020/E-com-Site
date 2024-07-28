4c4fb3f1c6934bf49af9f7df94dce211815d41af
atharva <atharva20.chirde@gmail.com>
2024-07-28 23:24:44 +0530

    7th commit

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,13 +1,100 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import Layout from "../components/Layout/Layout";
-import { useAuth } from "../context/auth";
+import { Prices } from "../components/Prices";
+import { Checkbox, Radio } from "antd";
 
 const HomePage = () => {
-  const [auth, setAuth] = useAuth();
+  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [checked, setChecked] = useState([]);
+  const [radio, setRadio] = useState([]);
+  const getAllCategory = async () => {
+    try {
+      const { data } = await axios.get("/api/v1/category/get-category");
+      if (data?.success) {
+        setCategories(data?.category);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getAllCategory();
+  }, []);
+  const getAllProducts = async () => {
+    try {
+      const { data } = await axios.get("/api/v1/product/get-product");
+      setProducts(data.products);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleFilter = (value, id) => {
+    let all = [...checked];
+    if (value) {
+      all.push(id);
+    } else {
+      all = all.filter((c) => c !== id);
+    }
+    setChecked(all);
+  };
+  useEffect(() => {
+    getAllProducts();
+  }, []);
   return (
-    <Layout>
-      <h1>HomePage</h1>
-      <pre>{JSON.stringify(auth, null, 4)}</pre>
+    <Layout title={"all products"}>
+      <div className="row">
+        <div className="col-md-2">
+          <h3 className="text-center"> Filter By Category</h3>
+          <div className="d-flex flex-column">
+            {categories?.map((c) => {
+              return (
+                <Checkbox
+                  key={c._id}
+                  onChange={(e) => handleFilter(e.target.checked, c._id)}
+                >
+                  {c.name}
+                </Checkbox>
+              );
+            })}
+          </div>
+          {/* filter by price */}
+          <h3 className="text-center mt-4"> Filter By Price</h3>
+          <div className="d-flex flex-column">
+            <Radio.Group onChange={(e) => setRadio(e.target.value)}>
+              {Prices?.map((p) => (
+                <div key={p._id}>
+                  <Radio value={p.array}>{p.name}</Radio>
+                </div>
+              ))}
+            </Radio.Group>
+          </div>
+        </div>
+        <div className="col-md-9">
+          {JSON.stringify(radio, null, 4)}
+          <h1 className="text-center">All Products</h1>
+          <div className="d-flex flex-wrap">
+            {products?.map((p) => (
+              <div className="card m-2" style={{ width: "18rem" }}>
+                <img
+                  src={`/api/v1/product/product-photo/${p._id}`}
+                  className="card-img-top"
+                  alt={p.name}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{p.name}</h5>
+                  <p className="card-text">{p.description}</p>
+                  <button class="btn btn-primary ms-1">See Details</button>
+                  <button class="btn btn-secondary ms-1">Add to Cart</button>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
